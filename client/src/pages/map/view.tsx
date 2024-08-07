06a1b020c44a86917feaa2b5f43ea96a02a24119
YG <yasha.golotin@mail.ru>
2024-08-07 20:59:44 +0300

    feat: styled login and map page, added react spring

--- client/src/pages/map/view.tsx
+++ client/src/pages/map/view.tsx
@@ -1,36 +1,62 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 
 export const Map: React.FC = () => {
+    const [ isModal, setIsModal ] = useState(false);
     const cnv  = useRef(null)
+
+    const handleModal = () => {
+        setIsModal(!isModal)
+    }
 
     return(
         <section className="w-screen h-screen flex flex-col justify-between" >
             <header className="flex flex-row justify-between">
-                <div className="w-10 h-10 border-2 rounded-b m-2 p-2 flex flex-col justify-around">
-                    <div className="border-2 border-2"></div>
-                    <div className="border-2 border-2"></div>
-                    <div className="border-2 border-2"></div>
+                <div className={isModal ?
+                    "absolute top-2 left-2 w-[300px] h-[300px] bg-amber-50 border rounded-xl"
+                    :
+                    "w-10 h-10 border-2 rounded-xl m-2 p-2 flex flex-col justify-around"
+                }
+                     onClick={handleModal}
+                >
+                    {
+                       isModal ?
+                           <div>
+                               <div>X</div>
+                               <div>
+                                   <button>Нстройки</button>
+                                   <button>Моя витрина</button>
+                                   <button>Выйти из аккаунта</button>
+                               </div>
+                           </div>
+                           :
+                           <>
+                               <div className="border-2 border-2"></div>
+                               <div className="border-2 border-2"></div>
+                               <div className="border-2 border-2"></div>
+                           </>
+                    }
+
                 </div>
                 <div className="m-2 flex flex-row items-center">
                     <div>
-                        <input type="text" className="border rounded-b p-2" placeholder='поиск витрин'/>
+                        <input type="text" className="border rounded-3xl p-2" placeholder='поиск витрин'/>
                     </div>
                 </div>
             </header>
             <main className="h-full flex flex-col items-center justify-between w-full">
-                <div className="w-full h-full  overflow-hidden flex gap-1">
-                    <section className="h-[100px] border flex items-center">
+                <div className="w-full h-full  overflow-hidden flex gap-2">
+                    <section className="h-[100px] border flex items-center rounded-xl">
                         <div className="w-20 h-20 m-2 border rounded-full">
                             <img src="" alt="usm"/>
                         </div>
                     </section>
-                    <section className="h-[100px] border flex items-center">
+                    <section className="h-[100px] border flex items-center rounded-xl">
                         <div className="w-20 h-20 m-2 border rounded-full">
                             <img src="" alt="usm"/>
                         </div>
                     </section>
                 </div>
-                <div className="border w-[350px] h-[500px]">
+                <div className="border w-[350px] h-[500px] rounded-2xl">
                     <div className="m-2 flex justify-around">
                         <div className="w-10 h-10 border rounded-full flex justify-center">
                             <button>1</button>
@@ -48,7 +74,7 @@ export const Map: React.FC = () => {
                             left
                             </button></div>
                         <div>
-                            <canvas ref={cnv} className="border w-[250px] h-[400px]"></canvas>
+                            <canvas ref={cnv} className="border rounded-xl w-[250px] h-[400px]"></canvas>
                         </div>
                         <div>
                             <button>
@@ -59,15 +85,15 @@ export const Map: React.FC = () => {
                 </div>
             </main>
             <footer>
-                <div className="h-[150px] m-2 p-2 border-2 rounded-b overflow-hidden flex flex-row gap-1">
-                    <section className="border w-[150px] p-2">
+                <div className="h-[150px] m-2 p-2 border-2 rounded-xl overflow-hidden flex flex-row gap-1">
+                    <section className="border w-[200px] p-2">
                         <h1>Витрина КБТ:</h1>
                         <p>Выстовить товар со склада</p>
                         <time>
                             04.08.2024
                         </time>
                     </section>
-                    <section className="border w-[150px] p-2">
+                    <section className="border w-[200px] p-2">
                         <h1>Витрина КБТ:</h1>
                         <p>Выстовить товар со склада</p>
                         <time>
