bd1ced39d011b535e8e21e96afcd52233bfc577d
TentakleM1 <tentakelme7@gmail.com>
2024-08-28 22:50:50 +0300

    feat: added endpoint, created store

--- client/src/shared/index.ts
+++ client/src/shared/index.ts
@@ -1,4 +1,1 @@
-export { HTTPTransport } from './HTTPTransport/HTTPTransoprt'
-export { BaseApi } from './api/base-api'
-export { UserApi } from './api/user-api/userApi'
-export { UserController } from './controller/user-controller/userController'
+export { store } from './store/store'
