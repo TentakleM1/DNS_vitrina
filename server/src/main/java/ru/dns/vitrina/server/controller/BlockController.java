078a40be89817382dda01fadad7cdb50298e9dcd
YG <yasha.golotin@mail.ru>
2024-08-29 22:33:42 +0300

    update: create BlockController

---
+++ server/src/main/java/ru/dns/vitrina/server/controller/BlockController.java
@@ -0,0 +1,28 @@
+package ru.dns.vitrina.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.dns.vitrina.server.model.Block;
+import ru.dns.vitrina.server.service.BlockService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping
+public class BlockController {
+    private final BlockService blockService;
+
+    @CrossOrigin
+    @GetMapping("/blocks")
+    public Map<Integer,Block> getBlocks() {
+        return blockService.getAllBlock();
+    }
+
+}
